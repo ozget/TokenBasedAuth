f3d90aaf0fc39776f58945669b4e2afd637695fc
ozget <toprakkozge@gmail.com>
2020-09-01 00:05:55 +0300

    Added Token startup file

---
+++ IdentityTokenBasedAuth/Security/Token/CustomTokenOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityTokenBasedAuth.Security.Token
+{
+    public class CustomTokenOptions
+    {
+        public string Audience { get; set; }//dinleyici
+        public string Issuer { get; set; }//dağıtıcı
+        public int AccessTokenExpiration { get; set; }
+        public int RefreshTokenexpiration { get; set; } //tokenın ömürü ile ilgili
+        public string SecuntyKey { get; set; }
+    }
+}
--- IdentityTokenBasedAuth/Startup.cs
+++ IdentityTokenBasedAuth/Startup.cs
@@ -13,6 +13,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting.Builder;
+using IdentityTokenBasedAuth.Security.Token;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
 
 namespace IdentityTokenBasedAuth
 {
@@ -31,7 +34,7 @@ namespace IdentityTokenBasedAuth
             services.AddControllers();
             var connectionString = Configuration["ConnectionString:DBConnectionString"];
             services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(connectionString));
-            
+
             services.AddIdentity<ApplicationUser, ApplicationRole>(opt =>
             {
                 opt.User.RequireUniqueEmail = true;
@@ -43,8 +46,33 @@ namespace IdentityTokenBasedAuth
                 opt.Password.RequireDigit = false;
 
             }).AddEntityFrameworkStores<ApplicationDbContext>();
-        
-        
+
+            services.Configure<CustomTokenOptions>(Configuration.GetSection("TokenOptions"));
+            var tokenOptions = Configuration.GetSection("TokenOptions").Get<CustomTokenOptions>();
+
+            //package manager : Microsoft.AspNetCore.Authentication.JwtBearer indirilmeli
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                //üyelik sistemi ile tokený birbirine baðlayan ortak bir þema belirtmemizi saðlýyor
+                //üyeler ve  bayiler olarak iki üye giriþi olduðunda birbirini ayýrt etmek için scheme yý kullanýyoruz. 
+
+            }).AddJwtBearer(jwtBearerOptions =>
+            jwtBearerOptions.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+            {
+                ValidateAudience = true,
+                ValidateIssuer = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = tokenOptions.Issuer,
+                ValidAudience = tokenOptions.Audience,
+                // IssuerSigningKey=SignHandler.
+                //uygun bir token olup olmadýgýnýn kontrolünü yapýyor
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
